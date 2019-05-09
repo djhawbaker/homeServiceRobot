a6e565ff690cd939ccc51f589e1a62da26c1bd00
David Hawbaker <djhawbaker@gmail.com>
2019-05-09 05:00:08 +0000

    Feat Added markers and improved object locations
    Added the markers package.
    Added the single object location test
    Improvedt he multiple pick object code

--- pick_objects/src/single_object.cpp
+++ pick_objects/src/single_object.cpp
@@ -1,5 +1,4 @@
 #include <ros/ros.h>
-#include "ros/time.h"
 #include <move_base_msgs/MoveBaseAction.h>
 #include <actionlib/client/simple_action_client.h>
 
@@ -26,7 +25,6 @@ int main(int argc, char** argv){
 
   // Define a position and orientation for the robot to reach
   goal.target_pose.pose.position.x = 1.0;
-  goal.target_pose.pose.position.y = 1.0;
   goal.target_pose.pose.orientation.w = 1.0;
 
    // Send the goal position and orientation for the robot to reach
@@ -38,29 +36,9 @@ int main(int argc, char** argv){
 
   // Check if the robot reached its goal
   if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
-    ROS_INFO("Hooray, the base moved to the goal");
+    ROS_INFO("Hooray, the base moved 1 meter forward");
   else
-    ROS_INFO("The base failed to move to the goal for some reason");
-
-  ros::Duration(5, 0).sleep();
-
-  // Define a position and orientation for the robot to reach
-  goal.target_pose.pose.position.x = 0.0;
-  goal.target_pose.pose.position.y = 0.0;
-  goal.target_pose.pose.orientation.w = 1.0;
-
-   // Send the goal position and orientation for the robot to reach
-  ROS_INFO("Sending goal to get home");
-  ac.sendGoal(goal);
-
-  // Wait an infinite time for the results
-  ac.waitForResult();
-
-  // Check if the robot reached its goal
-  if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
-    ROS_INFO("Hooray, the base moved back to home base");
-  else
-    ROS_INFO("The base failed to move back to home base for some reason");
+    ROS_INFO("The base failed to move forward 1 meter for some reason");
 
   return 0;
 }
---
+++ scripts/add_marker.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+# Start roscore
+xterm -e " source /opt/ros/kinetic/setup.bash; roscore " &
+sleep 5
+
+# Launch turtlebot
+#xterm -e " roslaunch turtlebot_gazebo turtlebot_world.launch " &
+xterm -e " roslaunch my_robot world.launch " &
+sleep 1
+
+# Launch AMCL
+xterm -e " roslaunch my_robot amcl.launch " &
+sleep 1
+
+# Launch Rviz
+xterm -e " roslaunch turtlebot_rviz_launchers view_navigation.launch " &
+sleep 5
+
+# Launch Add Markers 
+xterm -e " roslaunch add_markers add_markerslaunch " &
