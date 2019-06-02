1b0904f616b9a1671ee9f00421af578af9cb5365
David Hawbaker <djhawbaker@gmail.com>
2019-06-02 05:18:47 +0000

    Fix merged pick_objects launch into the view_navigation
    View_navigation launcher now provides parameters to the
    pick_objects node when launching it.

--- scripts/home_service.sh
+++ scripts/home_service.sh
@@ -13,14 +13,14 @@ sleep 1
 xterm -e " roslaunch my_robot localization.launch; bash " &
 sleep 5
 
-# Launch Rviz
-xterm -e " roslaunch my_robot view_navigation.launch; bash " &
-sleep 5
-
 # Launch Add Markers 
 xterm -e " rosrun add_markers add_markers; bash " &
 sleep 5
 
+# Launch Rviz and pick markers
+xterm -e " roslaunch my_robot view_navigation.launch; bash " &
+sleep 5
+
 # Launch Pick Markers
 #xterm -e " export ROS_IP=`hostname -I`; rosrun pick_objects pick_objects; bash " &
-xterm -e " rosrun pick_objects pick_objects; bash " &
+#xterm -e " rosrun pick_objects pick_objects; bash " &
