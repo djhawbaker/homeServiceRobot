0241dd8d703727b3ac308e40bb20c4e9ff3a82e5
David Hawbaker <djhawbaker@gmail.com>
2019-06-06 06:16:58 +0000

    Feat Started adding the MoveBaseServer
    The MoveBaseServer will tell the Client in pick_objects
    that the goal has been reached once the robot odom position
    matches the marker position.

--- add_markers/src/add_markers.cpp
+++ add_markers/src/add_markers.cpp
@@ -3,22 +3,45 @@
 #include "nav_msgs/Odometry.h"
 #include "geometry_msgs/Pose.h"
 #include "../include/marker.h"
+#include <move_base_msgs/MoveBaseAction.h>
+#include <actionlib/server/simple_action_server.h>
 #include "add_markers/AddMarker.h"
 #include "add_markers/RemoveMarker.h"
 
 //static Marker current_marker;
 bool found_marker;
+bool marker_set = false;
 ros::Publisher marker_pub;
+//ros::Publisher found_marker_pub;
 visualization_msgs::Marker marker;
+float MARKER_RADIUS = 0.1;
+typedef actionlib::SimpleActionServer<move_base_msgs::MoveBaseAction> MoveBaseServer;
+ros::NodeHandle nh;
 
 void odom_callback( const nav_msgs::Odometry odom )
 {
-  if ( odom.pose.pose.position.x == marker.pose.position.x &&
-       odom.pose.pose.position.y == marker.pose.position.x )
+  ROS_INFO("Odom callback x: %f, y: %f", marker.pose.position.x, marker.pose.position.y);
+  ROS_INFO("Odom pose : %f, y: %f", odom.pose.pose.position.x, odom.pose.pose.position.y);
+  if ( marker_set &&
+       ((marker.pose.position.x - MARKER_RADIUS ) <= odom.pose.pose.position.x )&&
+       (odom.pose.pose.position.x <= ( marker.pose.position.x + MARKER_RADIUS )) &&
+       (( marker.pose.position.y - MARKER_RADIUS ) <= odom.pose.pose.position.y) &&
+       (odom.pose.pose.position.y <= ( marker.pose.position.y + MARKER_RADIUS )))
   {
     // Found the marker
     found_marker = true;
     ROS_INFO("Found the marker");
+
+    // Publish that the marker was found
+    MoveBaseServer server(nh, "move_base");
+    move_base_msgs::MoveBaseResult result;
+    server.setSucceeded(result);
+
+
+    /* add_markers::FoundMarker foundMarker;
+    foundMarker.FOUND = true;
+    found_marker_pub.publish(foundMarker);
+    */
   }
 }
 
@@ -58,6 +81,7 @@ bool handle_add_marker_request(add_markers::AddMarker::Request& req, add_markers
     marker.type = visualization_msgs::Marker::CUBE;
 
     marker_pub.publish(marker);
+    marker_set = true;
 }
 
 bool handle_remove_marker_request(add_markers::AddMarker::Request& req, add_markers::AddMarker::Response& res)
@@ -65,6 +89,7 @@ bool handle_remove_marker_request(add_markers::AddMarker::Request& req, add_mark
   ROS_INFO("Remove Marker Request received");
 
   marker.action = visualization_msgs::Marker::DELETE;
+  marker_set = false;
   marker_pub.publish(marker);
 }
 
@@ -76,6 +101,7 @@ int main( int argc, char** argv )
   ros::NodeHandle n;
   ros::Rate r(1);
   marker_pub = n.advertise<visualization_msgs::Marker>("visualization_marker", 1);
+  //found_marker_pub = n.advertise<add_markers::FoundMarker>("found_marker", 1);
 
   ros::Subscriber odom_sub = n.subscribe("odom", 10, odom_callback);
   //ros::Subscriber marker = n.subscribe("visualization_marker", 10, marker_callback);
