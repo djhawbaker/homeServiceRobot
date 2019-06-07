25f3c7bd4d2ccdac5127e06021dc70616e613c8e
David Hawbaker <djhawbaker@gmail.com>
2019-06-07 07:23:56 +0000

    Fix Robot orientation, turning and rviz settings
    Increased the wheel torque for the robot to improve turning
    updated the initial pose orientation to better match the world
    updated the planner and costmap settings for the robot size

--- scripts/home_service.sh
+++ scripts/home_service.sh
@@ -9,18 +9,20 @@ xterm -e " roslaunch my_robot world.launch;" &
 sleep 5
 
 # Launch AMCL
+#xterm -e " roslaunch turtlebot_gazebo amcl.launch;" & # initial_pose_a:=1.5708;" &
 xterm -e " roslaunch my_robot amcl.launch initial_pose_a:=1.5708;" &
 #xterm -e " roslaunch my_robot localization.launch; bash " &
+#xterm -e "rosrun teleop_twist_keyboard teleop_twist_keyboard.py; bash" &
 sleep 5
+
+# Launch Rviz 
+xterm -e " roslaunch my_robot view_navigation.launch;" &
+sleep 10
 
 # Launch Add Markers 
 xterm -e " rosrun add_markers add_markers; bash" &
 sleep 5
 
-# Launch Rviz and pick markers
-xterm -e " roslaunch my_robot view_navigation.launch;" &
-sleep 5
-
 # Launch Pick Objects 
 #xterm -e " export ROS_IP=`hostname -I`; rosrun pick_objects pick_objects; bash " &
-xterm -e " rosrun pick_objects pick_objects;" &
+xterm -e " rosrun pick_objects pick_objects; bash" &
